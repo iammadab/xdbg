c8263b2b5be8592fbdf0bbf6bc786615636688f2
Wisdom Ogwu <iammadab@gmail.com>
2026-06-08 12:39:54 +0100

    add nix for ptrace interfacing

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// the goal here is to attach to a process
+// we can do this in two ways, first we could launch the program then attach
+// or we attach to an already existing program
+//
+// so we need to get command line inputs
+// then later make it better
+// here I have to handle the command line arguments
+//
+// xdbg program
+// xdbg -p PID
+
+fn main() {}
