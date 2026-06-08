8ffe322b69d1613ade5f11178f86b009a99b36a0
Wisdom Ogwu <iammadab@gmail.com>
2026-06-08 13:31:22 +0100

    clean up process attaching

--- src/main.rs
+++ src/main.rs
@@ -16,47 +16,61 @@ struct Cli {
     args: Vec<String>,
 }
 
-fn main() {
-    let cli = Cli::parse();
-
-    if let Some(pid) = cli.pid {
-        // parse the pid
-        if pid <= 0 {
-            panic!("invalid pid");
-        }
-        ptrace::attach(Pid::from_raw(pid)).unwrap();
-    } else if let Some(program) = cli.program {
-        // parse the program name
-        match unsafe { fork().unwrap() } {
-            ForkResult::Parent { child } => {
-                waitpid(child, None).unwrap();
-
-                // child is paused and we can inspect anything
-
-                todo!()
+fn attach(pid: Option<i32>, program: Option<String>, args: Vec<String>) -> Pid {
+    match (pid, program) {
+        // attach to the already running process
+        (Some(pid), None) => {
+            if pid <= 0 {
+                panic!("invalid pid");
             }
-            ForkResult::Child => {
-                // we need to exec the file path
-                // and call traceme
-                ptrace::traceme().unwrap();
+            let pid = Pid::from_raw(pid);
+            ptrace::attach(pid.clone()).unwrap();
+            pid
+        }
 
-                let filename = CString::new(program.as_str()).unwrap();
-                let mut exec_args = Vec::with_capacity(cli.args.len() + 1);
-                exec_args.push(program);
-                exec_args.extend(cli.args);
+        // launch the program then attach to the
+        // new child process
+        (None, Some(program_path)) => {
+            // fork the current process
+            match unsafe { fork().unwrap() } {
+                ForkResult::Parent { child } => child,
+                ForkResult::Child => {
+                    // tell the os that the parent is allowed to trace/debug
+                    // this child process
+                    ptrace::traceme().unwrap();
 
-                let c_args = exec_args
-                    .iter()
-                    .map(|arg| CString::new(arg.as_str()).unwrap())
-                    .collect::<Vec<_>>();
+                    // launch the program
+                    let filename = CString::new(program_path.as_str()).unwrap();
+                    let mut exec_args = Vec::with_capacity(args.len() + 1);
+                    exec_args.push(program_path);
+                    exec_args.extend(args);
 
-                match execvp(&filename, &c_args) {
-                    Ok(_) => unreachable!(),
-                    Err(err) => panic!("execvp failed: {err}"),
+                    let c_args = exec_args
+                        .into_iter()
+                        .map(|arg| CString::new(arg).unwrap())
+                        .collect::<Vec<_>>();
+
+                    match execvp(&filename, &c_args) {
+                        Ok(_) => unreachable!(),
+                        Err(err) => panic!("execvp failed: {err}"),
+                    }
                 }
             }
         }
-    } else {
-        panic!("expected --pid <PID> or <PROGRAM>");
+
+        // invalid arguments
+        _ => {
+            panic!("expected --pid <PID> or <PROGRAM>");
+        }
     }
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let pid = attach(cli.pid, cli.program, cli.args);
+
+    // wait for the child process
+    waitpid(pid, None).unwrap();
+
+    // can inspect child prcoess now
 }
